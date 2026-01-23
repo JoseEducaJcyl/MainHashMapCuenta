a7bf2452abe291638f11422baca4d939ef597bdf
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-23 10:31:46 +0100

    Ejercicio sobre palabras con un contador y un HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> palabras = new HashMap<>();
+
+        System.out.println("Ingrese un texto:");
+        String texto = sc.nextLine();
+
+        // Separar por comas
+        String[] partes = texto.split(",");
+
+        for (String palabra : partes) {
+            palabra = palabra.toLowerCase(); // opcional: evitar mayúsculas
+
+            if (palabras.containsKey(palabra)) {
+                palabras.put(palabra, palabras.get(palabra) + 1);
+            } else {
+                palabras.put(palabra, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : palabras.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
 }
