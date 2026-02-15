e236e3081266fc32ae26df32318acdefa5294246
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 16:06:44 +0100

    Comentarios agregados

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Declarmos un scanner
         Scanner sc = new Scanner(System.in);
+        //Declaramos un Map "palabras"
         Map<String, Integer> palabras = new HashMap<>();
 
+        //Le pedimos al usuario que introduzca un texto
         System.out.println("Ingrese un texto:");
         String texto = sc.nextLine();
 
-        // Separar por comas
+        //Se añade a un lista "partes" que se  
+        //separan por comas las partes del texto
         String[] partes = texto.split(",");
 
+        //Se recorre la lista y se cuenta cuantas veces 
+        //se repite la palabra 
         for (String palabra : partes) {
             palabra = palabra.toLowerCase(); // opcional: evitar mayúsculas
 
@@ -23,6 +29,7 @@ public class Main {
             }
         }
 
+        //Se muestra los resultados 
         for (Map.Entry<String, Integer> entry : palabras.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
